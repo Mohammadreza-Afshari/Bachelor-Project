32c55cd682273a382a82f198cebc0b8c8a9c42aa
Mohammadreza-Afshari <95334633+Mohammadreza-Afshari@users.noreply.github.com>
2022-09-09 08:41:42 -0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,9 +1,5 @@
 # convert mp3 to wav files
-
-#sound = AudioSegment.from_mp3(path.normpath('E:\\project\\folder\\clips\\common_voice_fa_18202354.mp3') )
-#sound.export('test.wav',format='wav')
-  
-    
+   
 from os import path
 import subprocess
 
